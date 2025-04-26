fc080e012ee08a2254e0d5d9c566ef119b1aa821
Nrzty <silviosantana2000@outlook.com>
2025-04-26 11:55:24 -0300

    feat: Added CRUD methods to CharacterService

--- src/main/java/br/com/nrzty/GenshinImpactAPI/Character/service/CharacterService.java
+++ src/main/java/br/com/nrzty/GenshinImpactAPI/Character/service/CharacterService.java
@@ -5,13 +5,39 @@ import br.com.nrzty.GenshinImpactAPI.Character.repository.CharacterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CharacterService {
 
     @Autowired
     CharacterRepository characterRepository;
 
+    public List<CharacterEntity> listCharacters(){
+        return characterRepository.findAll();
+    }
+
     public CharacterEntity createCharacter(CharacterEntity character) {
         return characterRepository.save(character);
+    }
+
+    public void deleteCharacter(CharacterEntity character) {
+        characterRepository.delete(character);
+    }
+
+    public CharacterEntity getCharacterById(Long id){
+        return characterRepository.findById(id).orElseThrow(() -> new RuntimeException("Character not found"));
+    }
+
+    public CharacterEntity updateCharacter(Long id, CharacterEntity character) {
+        CharacterEntity characterEntity = characterRepository.findById(id).orElseThrow(() -> new RuntimeException("Character not found"));
+
+        characterEntity.setName(character.getName());
+        characterEntity.setRegion(character.getRegion());
+        characterEntity.setVision(character.getVision());
+        character.setRarity(character.getRarity());
+        character.setWeaponType(character.getWeaponType());
+
+        return characterRepository.save(characterEntity);
     }
 }
